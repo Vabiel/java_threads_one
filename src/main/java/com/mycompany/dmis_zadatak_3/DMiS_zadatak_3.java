d3a1a65d927dd2d0f74fa453a7daba7e3fd6c697
SF97 <a.uchaev97@gmail.com>
2025-12-14 16:03:06 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/dmis_zadatak_3/DMiS_zadatak_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dmis_zadatak_3;
+
+/**
+ *
+ * @author vabiel
+ */
+public class DMiS_zadatak_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
